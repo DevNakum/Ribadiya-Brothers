0ced08d67690f2301555c8b7426992858d3bbfee
Priya <makadiapriya01@gmail.com>
2022-08-02 15:59:10 +0530

    inser icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {GiShoppingBag} from 'react-icons/gi';
+import {AiOutlineSearch} from 'react-icons/ai';
+
 
 function BasicExample() {
   return (
@@ -11,10 +14,16 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Home</Nav.Link>
             <Nav.Link href="#link">AboutUS</Nav.Link>
             <Nav.Link href="#link">ContactUS</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+            <Nav.Link href="#link"><AiOutlineSearch/></Nav.Link>
+            <Nav.Link href="#link"><GiShoppingBag/></Nav.Link>
+            
+            
           </Nav>
+         
         </Navbar.Collapse>
     </Navbar>
     </div>
